596ab359c9a2089fb95d17ef450eff83b1d66e04
Ahmed Hassan Mohi <a7med.hassan.mohi@gmail.com>
2024-01-27 12:14:48 +0200

    add navbar and footer

--- app/(marketing)/layout.tsx
+++ app/(marketing)/layout.tsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import Navbar from './_components/Navbar';
+import Footer from './_components/Footer';
 
 type Props = {
     children: React.ReactNode;
@@ -8,9 +10,11 @@ const MarketingLayout = ({children}: Props) => {
   return (
     <div className='h-full bg-slate-100'>
         {/* navbar */}
+        <Navbar/>
         <main className='pt-40 pb-20 bg-slate-100'>
         {children}
         </main>
+        <Footer/>
         {/* footer */}
     </div>
   )
